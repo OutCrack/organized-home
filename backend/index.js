98761c4cc94abcb4420ba16ca5fe36b383be5040
Andreas Stenseng Bjørnrud <andreas@bjornrud.com>
2023-06-16 16:45:50 +0200

    express setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port);
